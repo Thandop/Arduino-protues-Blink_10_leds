c432aecf99015a597fb777a0c4dc6adfa5693ecb
Proffessor Thando Mlotshwa <105253473+Thandop@users.noreply.github.com>
2023-09-13 20:51:07 +0200

    Add files via upload
    This file are for blinking 10 leds

---
+++ blink_10_lights.ino
@@ -0,0 +1,59 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(4,OUTPUT);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+   digitalWrite(13,HIGH);
+   delay(400);
+   digitalWrite(12,HIGH);
+   delay(400);
+   digitalWrite(11,HIGH);
+   delay(400);
+   digitalWrite(10,HIGH);
+   delay(400);
+   digitalWrite(9,HIGH);
+   delay(400);
+   digitalWrite(8,HIGH);
+   delay(400);
+   digitalWrite(7,HIGH);
+   delay(400);
+   digitalWrite(6,HIGH);
+   delay(400);
+   digitalWrite(5,HIGH);
+   delay(400);
+   digitalWrite(4,HIGH);
+   delay(400);
+  digitalWrite(13,LOW);
+   delay(400);
+   digitalWrite(12,LOW);
+   delay(400);
+   digitalWrite(11,LOW);
+   delay(400);
+   digitalWrite(10,LOW);
+   delay(400);
+   digitalWrite(9,LOW);
+   delay(400);
+   digitalWrite(8,LOW);
+   delay(400);
+   digitalWrite(7,LOW);
+   delay(400);
+   digitalWrite(6,LOW);
+   delay(400);
+   digitalWrite(5,LOW);
+   delay(400);
+   digitalWrite(4,LOW);
+   delay(400);
+
+}
